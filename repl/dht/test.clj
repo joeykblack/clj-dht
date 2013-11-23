e772a362d5a3dd6f03488abc17cb1c154484da16
joeykblack <joeykblack@gmail.com>
2013-11-23 16:06:56 -0500

    fixed logging

--- repl/dht/test.clj
+++ repl/dht/test.clj
@@ -3,27 +3,32 @@
 (use 'dht.net.serve)
 (use 'dht.net.send)
 (defonce server1 (make-node-server "http://localhost:3001" 3001))
-
 (defonce server2 (make-node-server "http://localhost:3002" 3002))
-(send-content "http://localhost:3001" {:add-node {:url "http://localhost:3002"}})
+
+(send-content-test "http://localhost:3001" {:add-node {:url "http://localhost:3002"}})
 
 (defonce server3 (make-node-server "http://localhost:3003" 3003))
+
 (send-content "http://localhost:3001" {:add-node {:url "http://localhost:3003"}})
 
 
-(send-content "http://localhost:3001" {:log-node-state {}})
-(send-content "http://localhost:3002" {:log-node-state {}})
-(send-content "http://localhost:3003" {:log-node-state {}})
+(send-content-test "http://localhost:3001" {:log-node-state {}})
+(send-content-test "http://localhost:3002" {:log-node-state {}})
+(send-content-test "http://localhost:3003" {:log-node-state {}})
 
-(send-content "http://localhost:3001" {:put-value {:key 'k2 :value 'v2}})
+(send-content-test "http://localhost:3001" {:put-value {:key 'k2 :value 'v2}})
 
 
 
-(send-content "http://localhost:3001" {:reset-node {:url "http://localhost:3001"}})
+(send-content-test "http://localhost:3001" {:reset-node {:url "http://localhost:3001"}})
 
 (.stop server1)
 (.stop server2)
 (.start server1)
+
+
+
+
 
 
 
--- src/dht/net/send.clj
+++ src/dht/net/send.clj
@@ -2,8 +2,10 @@
   (:use [clojure.tools.logging :only (info debug error)])
   (:require [clj-http.client :as client]))
 
+
 (defn send-content [url content]
   ;(debug url ":" content)
+  (info "Send:" content "to" url)
   (let [resp (client/post url {:form-params {:dht content}
                                :content-type :json})]
     (if (not= 200 (:status resp))
@@ -15,6 +17,14 @@
                                      :content-type :json})]
     (if (not= 200 (:status resp))
       (error resp))))
+
+(defn send-content-test [url content]
+  ;(debug url ":" content)
+  (let [resp (client/post url {:form-params {:dht content}
+                               :content-type :json})]
+    (if (not= 200 (:status resp))
+      (error resp))))
+
 
 
 
